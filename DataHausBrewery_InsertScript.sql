d62c3c6b0f8b106a646da7df1b1de8e4dc856b76
kydubois <kydubois28@gmail.com>
2019-02-26 21:38:14 -0800

    Add files via upload

---
+++ DataHausBrewery_InsertScript.sql
@@ -0,0 +1,22 @@
+
+INSERT INTO DataHaus_Brewery.BrewHaus.BREWERY VALUES ('1','DataHaus Brewery','123 Data Place','Datatown','OR','12345','8004567890','DataHausBrewery@gmail.com','https://www.datahausbrewery.com/');
+INSERT INTO DataHaus_Brewery.BrewHaus.BREWERY VALUES ('2','');
+INSERT INTO DataHaus_Brewery.BrewHaus.BREWERY VALUES ('3','');
+INSERT INTO DataHaus_Brewery.BrewHaus.BREWERY VALUES ('4','');
+INSERT INTO DataHaus_Brewery.BrewHaus.BREWERY VALUES ('5','');
+INSERT INTO DataHaus_Brewery.BrewHaus.BREWERY VALUES ('6','');
+INSERT INTO DataHaus_Brewery.BrewHaus.BREWERY VALUES ('7','');
+INSERT INTO DataHaus_Brewery.BrewHaus.BREWERY VALUES ('8','');
+INSERT INTO DataHaus_Brewery.BrewHaus.BREWERY VALUES ('9','');
+INSERT INTO DataHaus_Brewery.BrewHaus.BREWERY VALUES ('10','');
+
+INSERT INTO DataHaus_Brewery.BrewHaus.TAPROOM VALUES ('11','1','DataHaus Taproom','123 Data Place','Datatown','OR','12345','8004567890','DataHaus@gmail.com','https://www.datahausbrewery.com/','123456789');
+INSERT INTO DataHaus_Brewery.BrewHaus.TAPROOM VALUES ('12','2','Breakside Brewery - NW Slabtown','1570 NW 22nd Ave','Portland','OR','97210','5034447597','info@breakside.com','http://breakside.com/','89746546');
+INSERT INTO DataHaus_Brewery.BrewHaus.TAPROOM VALUES ('13','2','Breakside Brewery - NE Dekum Pub','820 Northeast Dekum Steet','Portland','OR','5037196475','info@breakside.com','http://breakside.com/','8976547');
+INSERT INTO DataHaus_Brewery.BrewHaus.TAPROOM VALUES ('14','3','Ex Novo Brewing Co. Pub','4505 SW Watson Ave','Beaverton','OR','97007','9712493737','info@exnovo.com','http://www.exnovobrew.com/beaverton','8965413645');
+INSERT INTO DataHaus_Brewery.BrewHaus.TAPROOM VALUES ('15','4','Mt. Hood Brewing Co.','87304 E Government Camp Loop','Government Camp','OR','97028','5032723172','https://mthoodbrewing.com/','23165489');
+INSERT INTO DataHaus_Brewery.BrewHaus.TAPROOM VALUES ('16','5','Left Hand Brewing Company Tasting Room','1265 Boston Ave','Longmont','CO','80501','','TastingRoom@lefthandbrewing.com','http://lefthandbrewing.com/brewery/tasting-room/','168153684');
+INSERT INTO DataHaus_Brewery.BrewHaus.TAPROOM VALUES ('17','6','Half Acre Beer Company - Lincoln Taproom','4257 N Lincoln Ave','Chicago','IL','60618','beer@halfacre.com','https://www.halfacrebeer.com/lincoln-tap-room','3564853');
+INSERT INTO DataHaus_Brewery.BrewHaus.TAPROOM VALUES ('18','7','Lagunitas Brewing Company - Chicago Taproom','2607 W 17th St','Chicago','IL','60608','info@lagunitas.com','7735222097','https://lagunitas.com/taproom/chicago','965258');
+INSERT INTO DataHaus_Brewery.BrewHaus.TAPROOM VALUES ('19','8','Cigar City Brewing = Tasting Room','3924 W Spruce St','Tampa','FL','33607','info@cigarcity.com','8133486363','https://cigarcitybrewing.com/tasting-room/tasting-room-hours/','321548465');
+INSERT INTO DataHaus_Brewery.BrewHaus.TAPROOM VALUES ('20','9','Rogue Pearl Public House','1339 NW Flander','Portland','OR','97209','','https://www.rogue.com/meeting-halls/rogue-distillery-and-public-house','215487451');
